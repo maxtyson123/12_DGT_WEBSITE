57e60fb24fa9d9354b99814e9a5b7345bf6621da
maxtyson123 <63089527+maxtyson123@users.noreply.github.com>
2021-12-25 18:14:06 +1300

    Buttons
    First i added a transition duration to make the site look better, then I tweaked it so it would work with js using    -webkit-transition: all 0.5s ease-in-out;
    
    After that I made a scroll to top button and move the JS code back into main.js (earlier it was in the html file because it wasn't detecting the window scrolling from the external file)
    
    Next I made a download button for windows and then if your device goes to mobile view the download button becomes andriod.

--- Website/JS/main.js
+++ Website/JS/main.js
@@ -1,14 +1,43 @@
 var toggle = 0;
+var menu = document.getElementById("MenuID");
+var dropdown = document.getElementById("DropDownID");
+var sticky = menu.offsetTop;
+
 function openMenuMobile() {
     var menuResponsive = document.getElementById("MenuID");
-   
 
-    if  (toggle == 0){
-    menuResponsive.className = "Menu responsive";
-    toggle = 1;
-    }
-    else{
+
+    if (toggle == 0) {
+        menuResponsive.className = "Menu responsive";
+        toggle = 1;
+    } else {
         menuResponsive.className = "Menu";
         toggle = 0;
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrolltotop.style.display = "block";
+    } else {
+        scrolltotop.style.display = "none";
+    }
+}
+
+
+function stickyMenu() {
+    if (window.pageYOffset >= sticky) {
+        menu.classList.add("sticky")
+
+        dropdown.classList.add("sticky-dropdown")
+    } else {
+        menu.classList.remove("sticky");
+        dropdown.classList.remove("sticky-dropdown");
+
     }
 }
