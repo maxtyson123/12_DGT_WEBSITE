643297e193751bfaddda30173375da7c4fc5bdad
maxtyson123 <63089527+maxtyson123@users.noreply.github.com>
2021-12-25 19:53:34 +1300

    Breif description and
    First I had to fix the scroll to top button not showing up by moving all the js that i previously moved back to the html file because it turns out it doesn't work externally that was just my browers loading weirdly.
    
    After that I added custom scroll bars to better fit the themeing.
    
    Next I began work on the brief description after adding tips to various buttons and links. For the brief I just center a <p> element and wrote directly into it which means I finally means I can remove the place holder text.
    
    Next I went and worked on the Images and bullet point  list for the features. I decided to go with columns and boxes.
    
    [STARTED VIDEO LOG]

--- Website/JS/main.js
+++ Website/JS/main.js
@@ -1,7 +1,5 @@
 var toggle = 0;
-var menu = document.getElementById("MenuID");
-var dropdown = document.getElementById("DropDownID");
-var sticky = menu.offsetTop;
+
 
 function openMenuMobile() {
     var menuResponsive = document.getElementById("MenuID");
@@ -13,31 +11,5 @@ function openMenuMobile() {
     } else {
         menuResponsive.className = "Menu";
         toggle = 0;
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrolltotop.style.display = "block";
-    } else {
-        scrolltotop.style.display = "none";
-    }
-}
-
-
-function stickyMenu() {
-    if (window.pageYOffset >= sticky) {
-        menu.classList.add("sticky")
-
-        dropdown.classList.add("sticky-dropdown")
-    } else {
-        menu.classList.remove("sticky");
-        dropdown.classList.remove("sticky-dropdown");
-
     }
 }
